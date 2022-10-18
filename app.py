f881a402edfd7ea7c71d5a131749ef4a2345f233
Migi Huang <mighuang2006@gmail.com>
2022-10-19 10:00:46 +1300

    Problems still

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def get_db_connection():
     conn.row_factory=sqlite3.Row
     return conn
 
-#This is using the books table and is for searching the Database for books only. 
+#This is using the books table and is for searching the Database for books only, by title author and date published
 @app.route('/', methods = ['POST', 'GET'])
 def home():
     
@@ -18,15 +18,17 @@ def home():
         
 
     return render_template ('home.html')
-            
+#not really important anymore          
 @app.route('/dad') 
 def dad():
     return render_template('dad.html')
 
+#Kinda important
 @app.route('/error') 
 def error():
     return render_template('error.html')
 
+#very important - where it redirects to after searching in the home page
 @app.route('/search/<term>')
 def search(term):
     conn = get_db_connection()
@@ -37,7 +39,7 @@ def search(term):
      
     return render_template('search.html', term=term, books=books)
 
-
+# displays all of the books in the database
 @app.route('/data')
 def data():
     conn = get_db_connection()
@@ -53,7 +55,7 @@ def hello():
     return render_template('add.html')
 
 
-
+#adding books, inserts the title, author and date published
 @app.route('/add', methods = ['POST', 'GET'])
 def add():
     if request.method == 'POST':
@@ -65,11 +67,12 @@ def add():
         conn.execute('INSERT INTO books (title, author, date_published) VALUES (?,?,?)', (add_title, add_author, add_date_published))   
         conn.commit()
         conn.close()
-        return redirect(url_for('data', title=add_title, author=add_author, date_published=add_date_published, add=add))
+        return redirect(url_for('data', title=add_title, author=add_author, date_published=add_date_published))
 
     
     return render_template('add.html')
 
+#deletes a book using the id, in case the same books is added twice, I'll make a thing says you cant add it if it already in the database
 @app.route('/delete', methods = ['POST', 'GET'])
 def delete():
     if request.method == 'POST':
@@ -83,26 +86,35 @@ def delete():
         return redirect(url_for('data'))
     return render_template('delete.html')
     
+#the hardedt bit so far
 @app.route('/borrow/<int:book_id>', methods = ['POST', 'GET'])
 def borrow(book_id):
     book_id=[(int(book_id))] 
     conn=get_db_connection()
     bookid=conn.execute('SELECT * from books WHERE idbooks is ?',(book_id))
+   
     conn.commit()
     conn.close()    
 
     if request.method == 'POST':
+        #for the borrowers part
         borrower_fname = request.form['fname']
         borrower_lname = request.form['lname']
         borrower_email = request.form['email']
         borrower_number = request.form['number']  
+        
+        conn=get_db_connection()
+        date=conn.execute('SELECT CURRENT_DATE')
+        borrower_insert=conn.execute('INSERT INTO borrowers (fname,lname,borrower_email,borrower_number) VALUES (?,?,?,?)', ((borrower_fname), (borrower_lname), (borrower_email), (borrower_number)))
+        conn.commit()
+        conn.close()
 
         conn=get_db_connection()
-        borrower=conn.execute('INSERT INTO borrowers (fname,lname,borrower_email,borrower_number) VALUES (?,?,?,?)', ((borrower_fname), (borrower_lname), (borrower_email), (borrower_number)))
-        #borrowed=conn.execute('DELETE FROM books where idbooks is ?',(book_id))
+        borrowerid=conn.execute('SELECT * from borrowers')
+        borrower_request=conn.execute('INSERT INTO borrowed_books(date_borrowed,books_idbooks, borrowers_idborrowers) VALUES(?,?,?)',((date)),((book_id), (borrowerid.idborrowers)))
         conn.commit() 
-        conn.close()
-        return redirect(url_for('thankyou', borrower=borrower)), #borrowed=borrowed))
+        conn.close()   
+        return redirect(url_for('thankyou', borrower_insert=borrower_insert, borrower_request=borrower_request))
 
     return render_template('borrow.html', bookid=bookid)
 
