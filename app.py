d312768cfe7bbeb07ca1029308501d1d5588ed17
Migi Huang <mighuang2006@gmail.com>
2022-11-03 10:27:48 +1300

    Not much progress made

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect, abort
 import sqlite3
+from datetime import datetime, timedelta
+
 
 app = Flask(__name__) 
 
@@ -87,9 +89,9 @@ def delete():
     return render_template('delete.html')
     
 #the hardedt bit so far
-@app.route('/borrow/<int:book_id>', methods = ['POST', 'GET'])
-def borrow(book_id):
-    book_id=[(int(book_id))] 
+@app.route('/borrow/<int:old_book_id>', methods = ['POST', 'GET'])
+def borrow(old_book_id):
+    book_id=[(int(old_book_id))]
     conn=get_db_connection()
     bookid=conn.execute('SELECT * from books WHERE idbooks is ?',(book_id))
    
@@ -101,20 +103,22 @@ def borrow(book_id):
         borrower_fname = request.form['fname']
         borrower_lname = request.form['lname']
         borrower_email = request.form['email']
-        borrower_number = request.form['number']  
+        borrower_number = request.form['number'] 
         
+        date=datetime.utcnow()
+        date_now=datetime.strftime(date,'%B %d %Y - %H:%M:%S')
         conn=get_db_connection()
-        date=conn.execute('SELECT CURRENT_DATE')
-        borrower_insert=conn.execute('INSERT INTO borrowers (fname,lname,borrower_email,borrower_number) VALUES (?,?,?,?)', ((borrower_fname), (borrower_lname), (borrower_email), (borrower_number)))
+        borrower_insert=conn.execute('INSERT INTO borrowers (fname,lname,borrower_email,borrower_number) VALUES (?,?,?,?)', (borrower_fname, borrower_lname, borrower_email, borrower_number))
         conn.commit()
-        conn.close()
+        row=conn.execute('SELECT idborrowers FROM borrowers WHERE fname=? AND lname=?',(borrower_fname, borrower_lname))
+        borrowers=row.fetchone()   
+        idborrowers=borrowers[0]
 
-        conn=get_db_connection()
-        borrowerid=conn.execute('SELECT * from borrowers')
-        borrower_request=conn.execute('INSERT INTO borrowed_books(date_borrowed,books_idbooks, borrowers_idborrowers) VALUES(?,?,?)',((date)),((book_id), (borrowerid.idborrowers)))
+        borrower_request=conn.execute('INSERT INTO borrowed_books(books_idbooks, borrowers_idborrowers, date_borrowed) VALUES(?,?,?)', (book_id, idborrowers, date_now))
         conn.commit() 
-        conn.close()   
+
         return redirect(url_for('thankyou', borrower_insert=borrower_insert, borrower_request=borrower_request))
+
 
     return render_template('borrow.html', bookid=bookid)
 
